0e832958da42051febbb322e8bb2f7be7a172b69
Oliver Blanthorn <freedom4cows@gmail.com>
2026-03-14 20:24:29 +0100

    Try doing it the right way with SPARQL

--- readme.md
+++ readme.md
@@ -9,3 +9,20 @@ todo:
 - find dictionary of my gauge enum -> compatible gauge enums
 - do some hacky maplibre / deck.gl (maybe?) web app where you can pick a gauge and see which routes are compatible and which are not
     - if there data is too big for maplibre i guess we'll have to convert it to geojson and tile it
+
+- spend a few minutes debugging why the sparql query misses out most of spain. if unfixable go back to julia method
+
+
+
+## sparql
+
+https://graph.data.era.europa.eu/sparql with the rinf database
+
+https://data-interop.era.europa.eu/endpoint has a playground that is less rubbish
+
+```sh
+curl -H "Accept: text/csv" \
+    -H "Content-Type: application/sparql-query" \
+    --data-binary @query.sparql \
+    https://graph.data.era.europa.eu/repositories/rinf
+```
--- wrangler.sql
+++ wrangler.sql
@@ -1,11 +1,16 @@
 -- duckdb
-INSTALL webbed FROM community;
-LOAD webbed;
+INSTALL spatial;
+LOAD spatial;
 
-SELECT UniqueOPID, OPGeographicLocation FROM read_xml(
-    'data/20260310_RINF_RFI.xml',
-    maximum_file_size = 2147483648,
-    record_element = 'OperationalPoint'
-)
---WHERE OPGeographicLocation IS NOT NULL
-LIMIT 1;
+-- get some error about nanoarrow so use parquet for now
+-- although really i dunno why i am using duckdb, i should have just used clickhouse like always
+-- INSTALL arrow FROM community;
+-- LOAD arrow;
+select * from 'out.csv' limit 1;
+COPY (
+    select start_lat, end_lat, first(start_lon) start_lon, first(end_lon) end_lon, min(gp) gp, argmin(gpLabel,gp) gpLabel from (
+        select sol, ST_X(start_op) start_lon, ST_Y(start_op) start_lat, ST_X(end_op) end_lon, ST_Y(end_op) end_lat, gpLabel, list_last(string_split(gp, '/'))::USMALLINT gp from (
+            select sol, ST_GeomFromText(startWkt) start_op, ST_GeomFromText(endWkt) end_op, gp, gpLabel from 'out.csv'
+        )
+    ) group by sol, start_lat, end_lat
+) TO 'out.parquet' (FORMAT 'parquet');
